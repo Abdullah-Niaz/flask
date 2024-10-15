b24d42d9d041434c32a8d41a0678f0db93427763
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-10-15 15:48:35 +0500

    http request

---
+++ Working With HTTP Verbs Get And Post/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['username']
+        password = request.form['password']
+        print(name)
+        print(password)
+        return render_template('successfullSubmission.html', name=name, password=password)
+    return render_template('form.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
